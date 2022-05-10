8bedf9b99497fdb94f48e229a08868664a8e4011
scarabele <henriquescarabele@hotmail.com>
2022-05-09 23:47:48 -0300

    fix: remove comment tests

--- test/calculator.test.js
+++ test/calculator.test.js
@@ -4,7 +4,7 @@ const calculator = new Calculator();
 test('adds 1 + 2 to equal 3', () => {
   expect(calculator.sum(1, 2)).toBe(3);
 });
-/*
+
 test('subtracts 3 - 1 to equal 2', () => {
   expect(calculator.minus(3, 1)).toBe(2);
 });
@@ -24,4 +24,3 @@ test('adds array of numbers to equal 6', () => {
 test('multiplies array of numbers to equal 12', () => {
   expect(calculator.multiplyOfArray([2, 2, 3])).toBe(12);
 });
-*/
